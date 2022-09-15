980f8f32a027aac9aa112a647c871a3ff30fe9f2
David Venegas <venegasdavidm@gmail.com>
2022-09-15 16:01:54 -0400

    playgrounc

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,32 @@
-import * as Rx from "rxjs/internal/observable"
+import { Observable } from "rxjs/internal/observable"
 
-console.log(Rx)
+var observable = new Observable((observer: any) => {
+    try {
+        observer.next("Hey guys")
+        observer.next("How are you?")
+        setInterval(() => (
+            observer.next("I am good")
+        ), 2000)
+    } catch(e) {
+        observer.error(e)
+    }
+})
+
+var observer = observable.subscribe(
+    (x: any) => addItem(x),
+    (error) => addItem(error),
+    () => addItem('/end/')
+)
+
+setTimeout(() => {
+    var observer2 = observable.subscribe(
+        (x: any) => addItem('Subscriber 2: ' + x), 
+    )
+}, 1000);
+
+function addItem(input: any) {
+    var node = document.createElement("li")
+    var text = document.createTextNode(input)
+    node.appendChild(text)
+    document.getElementById("output").appendChild(node)
+}
